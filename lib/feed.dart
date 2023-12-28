c2182e49a23214eed662d403d945193fb5622764
이정원 <jung_won@ijeong-won-ui-noteubug.local>
2023-12-28 13:36:55 +0900

    Feed + icons추가

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainFeed extends StatelessWidget {
@@ -11,7 +12,33 @@ class MainFeed extends StatelessWidget {
       children: [
         Image.network(
           "https://i.ibb.co/YjjLCS7/cat.png",
-        )
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.heart,
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.chat_bubble,
+                color: Colors.black,
+              ),
+            ),
+            const Spacer(), // 빈 공간 추가
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.bookmark,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
